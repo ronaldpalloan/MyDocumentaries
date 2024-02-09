535e4e4fae6814cbbd7b0ae82053f96c2eb11875
Ronald Palloan <rpalloan0@gmail.com>
2024-02-09 21:15:22 +0700

    fitur finish, remove wantwatch

--- script.js
+++ script.js
@@ -105,12 +105,14 @@ form.addEventListener('submit', function(e) {
 		<p class="per-channel">${formChannel.value}</p>
 		<p class="per-duration">${formHours.value}:${formMinutes.value}:${formSeconds.value}</p>
 		<span class="watch-to-finished">
-			<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle" viewBox="0 0 16 16">
+			<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle finished-wantwatch-button" viewBox="0 0 16 16">
+				<title id="finished-title">Finished</title>
 			  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
 			  <path d="m10.97 4.97-.02.022-3.473 4.425-2.093-2.094a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05"/>
 			</svg>
 
-			<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
+			<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle remove-wantwatch-button" viewBox="0 0 16 16">
+				<title id="delete-title">Remove</title>
 			  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
 			  <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
 			</svg>
@@ -119,7 +121,7 @@ form.addEventListener('submit', function(e) {
 		newWantWatch.innerHTML = perWantWatch;
 		wantWatchContainer.insertBefore(newWantWatch, wantWatchContainer.firstChild);
 
-		// saveData();
+		saveData();
 	}
 
 	form.reset();
@@ -242,80 +244,92 @@ window.addEventListener('click', function(e) {
 	// Tampilkan
 	if (e.target.classList.contains('finished-wantwatch-button')) {
 		finishWantwatchContainer.style.display = 'flex';
+		// Isi Data sesuai
+		const titleFinishWantwatch = document.getElementById('titleFinishWantwatch');
+		const channelFinishWantwatch = document.getElementById('channelFinishWantwatch');
+		const durationFinishWantwatch = document.getElementById('durationFinishWantwatch');
+		const dateFinishWantwatch = document.getElementById('dateFinishWantwatch');
+		const ratingFinishWantwatch = document.getElementById('ratingFinishWantwatch');
+		const parentWantwatch = e.target.parentElement.parentElement;
+
+		valueTitleWantwatch = parentWantwatch.querySelector('.per-title').innerText;
+		valueChannelWantwatch = parentWantwatch.querySelector('.per-channel').innerText;
+		valueDurationWantwatch = parentWantwatch.querySelector('.per-duration').innerText;
+
+		titleFinishWantwatch.value = valueTitleWantwatch;
+		channelFinishWantwatch.value = valueChannelWantwatch;
+		durationFinishWantwatch.value = valueDurationWantwatch;
+		dateFinishWantwatch.value = new Date().toISOString().slice(0, 10);
+
+		// Validasi Input Rating
+		ratingFinishWantwatch.addEventListener('input', function() {
+			ratingFinishWantwatch.value = ratingFinishWantwatch.value.replace(/[^0-9]/g, '');
+			parsedRating = parseInt(ratingFinishWantwatch.value, 10);
+			if (isNaN(parsedRating)) {
+				ratingFinishWantwatch.value = '';
+			} else {
+				ratingFinishWantwatch.value = Math.min(Math.max(parsedRating, 1), 10);
+			}
+		});
+	} else if (e.target.classList.contains('remove-wantwatch-button')) {
+		e.target.parentElement.parentElement.remove();
+		saveData();
 	}
-	// Isi Data sesuai
-	const titleFinishWantwatch = document.getElementById('titleFinishWantwatch');
-	const channelFinishWantwatch = document.getElementById('channelFinishWantwatch');
-	const durationFinishWantwatch = document.getElementById('durationFinishWantwatch');
-	const dateFinishWantwatch = document.getElementById('dateFinishWantwatch');
-	const ratingFinishWantwatch = document.getElementById('ratingFinishWantwatch');
-	const parentWantwatch = e.target.parentElement.parentElement;
+});
 
-	valueTitleWantwatch = parentWantwatch.querySelector('.per-title').innerText;
-	valueChannelWantwatch = parentWantwatch.querySelector('.per-channel').innerText;
-	valueDurationWantwatch = parentWantwatch.querySelector('.per-duration').innerText;
+// Tombol Finish di form Wantwatch
+const finishWantwatchButton = document.getElementById('finishWantwatchButton');
 
-	titleFinishWantwatch.value = valueTitleWantwatch;
-	channelFinishWantwatch.value = valueChannelWantwatch;
-	durationFinishWantwatch.value = valueDurationWantwatch;
-	dateFinishWantwatch.value = new Date().toISOString().slice(0, 10);
+finishWantwatchButton.addEventListener('click', function(e) {
+	const newDataFinished = document.createElement('div');
+	newDataFinished.classList.add('perDocumentary');
+	const documentaryOrder = containerAllDocumentary.childElementCount;
 
-	// Validasi Input Rating
-	ratingFinishWantwatch.addEventListener('input', function() {
-		ratingFinishWantwatch.value = ratingFinishWantwatch.value.replace(/[^0-9]/g, '');
-		parsedRating = parseInt(ratingFinishWantwatch.value, 10);
-		if (isNaN(parsedRating)) {
-			ratingFinishWantwatch.value = '';
-		} else {
-			ratingFinishWantwatch.value = Math.min(Math.max(parsedRating, 1), 10);
+	const perDocumentary = `
+		<p class="per-number">${documentaryOrder + 1}</p>
+		<div class="documentaryInfo">
+			<p class="per-title">${titleFinishWantwatch.value}</p>
+			<p class="per-channel">${channelFinishWantwatch.value}</p>
+			<p class="per-duration">${durationFinishWantwatch.value}</p>
+		</div>
+		<div class="rating-container-finished">
+			<div class="documentaryRating">
+				<img src="images/star.png" alt="">
+				<p class="per-rating">${ratingFinishWantwatch.value}</p>
+			</div>
+			<p class="per-date">(${dateFinishWantwatch.value})</p>
+		</div>
+		<div class="edit-finished">
+			<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-down-left-circle edit-finished-button" viewBox="0 0 16 16">
+			  <title id="edit-title">Edit</title>
+			  <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-5.904-2.854a.5.5 0 1 1 .707.708L6.707 9.95h2.768a.5.5 0 1 1 0 1H5.5a.5.5 0 0 1-.5-.5V6.475a.5.5 0 1 1 1 0v2.768z"/>
+			</svg>
+			<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle remove-finished-button" viewBox="0 0 16 16">
+			  <title id="delete-title">Remove</title>
+			  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+			  <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
+			</svg>
+		</div>`;
+
+	newDataFinished.innerHTML = perDocumentary;
+	containerAllDocumentary.insertBefore(newDataFinished, containerAllDocumentary.firstChild);
+
+	finishWantwatchContainer.style.display = 'none';
+
+	// Remove after Finish
+	const allWantwatchTitle = Array.from(document.querySelectorAll('.per-wantwatch .per-title'));
+	for (item of allWantwatchTitle) {
+		if (item.innerText === titleFinishWantwatch.value) {
+			item.parentElement.remove();
 		}
-	});
+	}
 
-	// Tekan Tombol Finish
-	const finishWantwatchButton = document.getElementById('finishWantwatchButton');
+	// Agar Rating Reset setelah finish
+	ratingFinishWantwatch.value = '';
 
-	finishWantwatchButton.addEventListener('click', function(e) {
-		const newDataFinished = document.createElement('div');
-		newDataFinished.classList.add('perDocumentary');
-		const documentaryOrder = containerAllDocumentary.childElementCount;
-
-		const perDocumentary = `
-			<p class="per-number">${documentaryOrder + 1}</p>
-			<div class="documentaryInfo">
-				<p class="per-title">${titleFinishWantwatch.value}</p>
-				<p class="per-channel">${channelFinishWantwatch.value}</p>
-				<p class="per-duration">${durationFinishWantwatch.value}</p>
-			</div>
-			<div class="rating-container-finished">
-				<div class="documentaryRating">
-					<img src="images/star.png" alt="">
-					<p class="per-rating">${ratingFinishWantwatch.value}</p>
-				</div>
-				<p class="per-date">(${dateFinishWantwatch.value})</p>
-			</div>
-			<div class="edit-finished">
-				<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-down-left-circle edit-finished-button" viewBox="0 0 16 16">
-				  <title id="edit-title">Edit</title>
-				  <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-5.904-2.854a.5.5 0 1 1 .707.708L6.707 9.95h2.768a.5.5 0 1 1 0 1H5.5a.5.5 0 0 1-.5-.5V6.475a.5.5 0 1 1 1 0v2.768z"/>
-				</svg>
-				<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle remove-finished-button" viewBox="0 0 16 16">
-				  <title id="delete-title">Remove</title>
-				  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
-				  <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
-				</svg>
-			</div>`;
-
-		newDataFinished.innerHTML = perDocumentary;
-		containerAllDocumentary.insertBefore(newDataFinished, containerAllDocumentary.firstChild);
-
-		finishWantwatchContainer.style.display = 'none';
-
-
-		e.preventDefault();
-		// saveData();
-	})
-	parentWantwatch.remove();
-})
+	e.preventDefault();
+	saveData();
+});
 
 
 // Tombol Close pada Finish Wantwatch
@@ -345,7 +359,7 @@ function saveData() {
 
 function showData() {
 	containerAllDocumentary.innerHTML = localStorage.getItem('latihanDoc');
-	// wantWatchContainer.innerHTML = localStorage.getItem('wantwatchdocumentaries');
+	wantWatchContainer.innerHTML = localStorage.getItem('wantwatchdocumentaries');
 }
 
 
